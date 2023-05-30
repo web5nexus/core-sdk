109549823cc1c90966b1f027b5d9a5fd7c879a9d
Jetso Analin <jetyjetso@gmail.com>
2023-05-30 19:04:50 +0530

    Web5 Auth: Basic Social and Passwordless Auth Initiated

--- packages/web3auth-core/src/index.ts
+++ packages/web3auth-core/src/index.ts
@@ -1,1 +1,4 @@
-export { init,facebook } from './social'
+export {facebook,google,twitch,twitter,discord,apple,github,linkedin,reddit } from './social'
+export {web5auth} from './initialize'
+export {email,mobile} from './passwordless'
+export {walletconnect} from './walletconnect'
