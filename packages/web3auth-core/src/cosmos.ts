3276a7114e589b0b38c45745779765d65666cfcd
Jetso Analin <jetyjetso@gmail.com>
2023-06-03 16:51:18 +0530

    Cosmos chain: Balance Fetch Function Initiated

--- packages/web3auth-core/src/cosmos.ts
+++ packages/web3auth-core/src/cosmos.ts
@@ -4,24 +4,52 @@ import { bech32 } from "bech32";
 import type { SafeEventEmitterProvider } from "@web3auth/base";
 import Web3 from "web3";
 import cosmoschains from 'cosmoschains';
+import { Secp256k1HdWallet, Secp256k1Wallet,makeCosmoshubPath } from "@cosmjs/amino";
+import { Secp256k1Signature, Secp256k1 } from "@cosmjs/crypto";
+import { StargateClient, SigningStargateClient, GasPrice } from "@cosmjs/stargate";
+import { fromHex,fromBase64 } from "@cosmjs/encoding";
 
 export default class CosmosRpc {
     public hrp: string;
     public blockchain: string;
     private provider: SafeEventEmitterProvider;
+    public rpcTarget: string;
+    public client:any;
 
     constructor(blockchain: string, network: string, provider: SafeEventEmitterProvider) {
         this.blockchain = blockchain;
         this.hrp = this.getHrpValue(blockchain, network);
         this.provider = provider;
+        this.rpcTarget = this.getRpcValue(blockchain, network);
+    }
+
+    private async initiate():Promise<any>{
+        try {
+            this.client = await StargateClient.connect(this.rpcTarget);
+            return this.client;
+        } catch (error) {
+            return error as string;
+        }
     }
 
     private getHrpValue(blockchain: string, network: string): string {
+        blockchain = blockchain.toLowerCase();
+        network = network.toLowerCase();
         const chain = cosmoschains.find(chain => chain.blockchain === blockchain && chain.network === network);
         if (chain) {
           return chain.hrp;
         }
         throw new Error(`Could not find hrp value for the specified blockchain (${blockchain}) and network (${network}) combination.`);
+      }
+
+      private getRpcValue(blockchain: string, network: string): string {
+        blockchain = blockchain.toLowerCase();
+        network = network.toLowerCase();
+        const chain = cosmoschains.find(chain => chain.blockchain === blockchain && chain.network === network);
+        if (chain) {
+          return chain.rpcTarget;
+        }
+        throw new Error(`Could not find RPC Target value for the specified blockchain (${blockchain}) and network (${network}) combination.`);
       }
 
     private async CosmosBufferToAddress(pubBuf: any) {
@@ -50,6 +78,33 @@ export default class CosmosRpc {
         } catch (error) {
             console.error(error);
             throw error;
+        }
+    }
+
+    async getBalance(): Promise<string> {
+        try {
+            const client = await this.initiate();
+
+            // Get user's Cosmos public address
+            const address = await this.getAccounts();
+            const balance = await client.getAllBalances(address);
+
+            return balance;
+        } catch (error) {
+            return error as string;
+        }
+    }
+
+    async getChainId(): Promise<string> {
+        try {
+            const client = await this.initiate();
+
+            // Get the connected Chain's ID
+            const chainId = await client.getChainId();
+
+            return chainId.toString();
+        } catch (error) {
+            return error as string;
         }
     }
 
--- packages/web3auth-core/src/cosmoschains.ts
+++ packages/web3auth-core/src/cosmoschains.ts
@@ -2,21 +2,25 @@ export interface CosmosChainConfig {
   blockchain: string;
   hrp: string;
   network: string;
+  rpcTarget: string;
 }
 
 const cosmoschains: CosmosChainConfig[] = [
   {
     blockchain: "coreum",
     hrp: "testcore",
-    network: "testnet"
+    network: "testnet",
+    rpcTarget: "https://full-node.testnet-1.coreum.dev:2665"
   }, {
     blockchain: "coreum",
     hrp: "core",
-    network: "mainnet"
+    network: "mainnet",
+    rpcTarget: "https://full-node.mainnet-1.coreum.dev:26657"
   },{
     blockchain: "cosmos",
     hrp: "cosmos",
-    network: "mainnet"
+    network: "mainnet",
+    rpcTarget: "https://full-node.testnet-1.coreum.dev:2665"
   },
 ];
 
